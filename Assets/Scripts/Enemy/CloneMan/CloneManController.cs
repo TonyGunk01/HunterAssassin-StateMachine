17dd0c65cdb8a5857de52c2d8d203cd8d476446a
TonyGunk01 <rishivl@hotmail.com>
2025-12-21 11:45:49 +0530

    Creating Clone-Man
    Created CloneManStateMachine.cs in /Scripts/Enemy/CloneMan.
    
    Used GenericStateMachine class for handling state management for CloneManStateMachine.
    
    Created CloneManController.cs in /Scripts/Enemy/CloneMan/ to represent new enemies.
    
    Created a new Level and add multiple Clone Man in this Level.

---
+++ Assets/Scripts/Enemy/CloneMan/CloneManController.cs
@@ -0,0 +1,45 @@
+﻿using StatePattern.Enemy;
+using StatePattern.Player;
+using StatePattern.StateMachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Enemy
+{
+    public class CloneManController : EnemyController
+    {
+        private CloneManStateMachine stateMachine;
+
+        public CloneManController(EnemyScriptableObject enemyScriptableObject) : base(enemyScriptableObject)
+        {
+            enemyView.SetController(this);
+            CreateStateMachine();
+            stateMachine.ChangeState(States.IDLE);
+        }
+
+        private void CreateStateMachine() => stateMachine = new HitmanStateMachine(this);
+
+        public override void UpdateEnemy()
+        {
+            if (currentState == EnemyState.DEACTIVE)
+                return;
+
+            stateMachine.Update();
+        }
+
+        public override void Shoot()
+        {
+            base.Shoot();
+            stateMachine.ChangeState(States.TELEPORTING);
+        }
+
+        public override void PlayerEnteredRange(PlayerController targetToSet)
+        {
+            base.PlayerEnteredRange(targetToSet);
+            stateMachine.ChangeState(States.CHASING);
+        }
+
+        public override void PlayerExitedRange() => stateMachine.ChangeState(States.IDLE);
+    }
+}
---
+++ Assets/Scripts/Enemy/CloneMan/CloneManStateMachine.cs
@@ -0,0 +1,26 @@
+﻿using StatePattern.Enemy;
+using StatePattern.StateMachine;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Enemy
+{
+    public class CloneManStateMachine : GenericStateMachine<CloneManController>
+    {
+        public CloneManStateMachine(HitmanController Owner) : base(Owner)
+        {
+            this.Owner = Owner;
+            CreateStates();
+            SetOwner();
+        }
+
+        private void CreateStates()
+        {
+            States.Add(StateMachine.States.IDLE, new IdleState<CloneManController>(this));
+            States.Add(StateMachine.States.PATROLLING, new PatrollingState<CloneManController>(this));
+            States.Add(StateMachine.States.CHASING, new ChasingState<CloneManController>(this));
+            States.Add(StateMachine.States.SHOOTING, new ShootingState<CloneManController>(this));
+            States.Add(StateMachine.States.TELEPORTING, new TeleportingState<CloneManController>(this));
+        }
+    }
+}
